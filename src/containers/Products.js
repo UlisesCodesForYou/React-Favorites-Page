dbbbe54950c2e25be8e5990ceba388244b035e59
UlisesCodesForYou <76757797+UlisesCodesForYou@users.noreply.github.com>
2022-05-24 21:34:58 -0700

    Began to replace redux with hooks.

--- src/containers/Products.js
+++ src/containers/Products.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React ,{useContext}from 'react';
+
 
 import ProductItem from '../components/Products/ProductItem';
+import {ProductsContext} from '../context/product-context'
 import './Products.css';
 
 const Products = props => {
-  const productList = useSelector(state => state.shop.products);
+  const ctx = useContext(ProductsContext).products
   return (
     <ul className="products-list">
-      {productList.map(prod => (
+      {ctx.map(prod => (
         <ProductItem
           key={prod.id}
           id={prod.id}
